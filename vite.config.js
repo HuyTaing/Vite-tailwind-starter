8713931ba0751994dbfa5a9affdb10564796a427
TAING Huy <taing.huy@outlook.fr>
2022-11-10 00:52:39 +0100

    fichier .html déplacer dans le racine et changement de source des html sur vite config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        about: resolve(__dirname, 'src/about.html'),
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
       },
     },
   },
